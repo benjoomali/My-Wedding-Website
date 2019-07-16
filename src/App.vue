4f5473e05800f33f5f38b2ce7ce59344d112f809
Ben Umali <benjoomali@gmail.com>
2019-07-16 07:58:46 -0700

    changed text in some sections

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,82 @@
 <template>
   <div id="app">
-      <ul id="menu">
-          <li data-menuanchor="page1" class="active"><a href="#page1">Section 1</a></li>
-          <li data-menuanchor="page2"><a href="#page2">Section 2</a></li>
-          <li data-menuanchor="page3"><a href="#page3">Section 3</a></li>
-      </ul>
-
       <full-page ref="fullpage" :options="options" id="fullpage" >
           <div class="section">
-              <h3>Section 1</h3>
+              <h3>The Ceremony</h3>
+              <p>St. Andrew Church in Pasadena</p>
+              <p>311 N Raymond Ave Pasadena, CA</p>
+              <p>12:30pm</p>
           </div>
           <div class="section">
-              <div class="slide">
-                  <h3>Slide 2.1</h3>
-              </div>
-              <div class="slide">
-                  <h3>Slide 2.2</h3>
-              </div>
-              <div class="slide">
-                  <h3>Slide 2.3</h3>
-              </div>
+                <h3>The Reception</h3>
+                <p>Glen Arden Club in Glendale</p>
+                <p>357 Arden Ave Glendale, CA</p>
+                <p>4:30pm</p>
           </div>
           <div class="section">
-              <h3>Section 3</h3>
+              <h3>Our Story</h3>
+              <p>Benjamin and Danielle first met in 2005 through a Catholic youth group called Couples For Christ Youth and Danielle was hosting a Halloween party at her house. Little did she know that her future husband was going to be attending this party. After many years of being friends, Danielle started her first year of college at Cal Poly Pomona while Benjamin stayed behind in Bakersfield to finish his senior year in high school. All the while, they were still one of each other's best friends throughout the year. Once Benjamin graduated high school and Danielle came back home for the summer, they began spending more time together over the summer. When Benjamin started college at University of California, Irvine in 2009, he asked Danielle's parents if he could court her. After many years of pursuing, Benjamin and Danielle continued to grow as friends, best friends and as partners. On January 27, 2018, Benjamin asked Danielle for her hand in marriage...and the rest is history!</p>
+          </div>
+          <div class="section">
+              <h3>Hotel Accomodations</h3>
+              <a href="https://www.hilton.com/en/hi/groups/personalized/B/BURHGHF-BDW-20191011/index.jhtml?WT.mc_id=POG" target="_blank">Click to Book</a>
+          </div>
+          <div class="section">
+              <h3>Our Gift Registry</h3>
+              <a href="https://www.zola.com/registry/danielleandbengetmarried" target="_blank">View our Zola Gift Registry</a>
+              <p>We would love to give all our guests the opportunity to let their hair down and have a good time without having to worry about little eyes and ears, so we politely request <strong>no children.</strong></p>
+          </div>
+          <div class="section">
+            <h3>Our Gallery</h3>
+
           </div>
       </full-page>
   </div>
 </template>
 
 <script>
-
+import VueGallery from 'vue-gallery';
 export default {
   name: 'app',
-  options: {
-    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-    scrollBar: false,
-    menu: '#menu',
-    navigation: true,
-    anchors: ['ceremony', 'reception', 'story','hotel','registry'],
-    sectionsColor: ['#41b883', '#ff5f45', '#0798ec', '#fec401', '#1bcee6', '#ee1a59', '#2c3e4f', '#ba5be9', '#b4b8ab']
+  components: {
+  'gallery': VueGallery
   },
+  data () {
+    return {
+    options: {
+      licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+      scrollBar: false,
+      menu: '#menu',
+      navigation: true,
+      anchors: ['ceremony', 'reception', 'story', 'hotel', 'registry', 'gallery'],
+      sectionsColor: ['#41b883', '#ff5f45', '#0798ec', '#fec401', '#1bcee6', '#a36b4f']
+    },
+    images: [
+      {
+        title: '400x400',
+        description: '400 x 400 White',
+        href: 'https://dummyimage.com/400/ffffff/000000',
+      },
+      {
+        title: '400x400',
+        description: '400 x 400 White',
+        href: 'https://dummyimage.com/400/ffffff/000000',
+      },
+      {
+        title: '400x400',
+        description: '400 x 400 Black',
+        href: 'https://dummyimage.com/400/000000/ffffff',
+      },
+      {
+        title: '400x400',
+        description: '400 x 400 Black',
+        href: 'https://dummyimage.com/400/000000/ffffff',
+      },
+    ],
+    index: null
+    }
+  },
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
 import VueFullPage from 'vue-fullpage.js'
+import VueGallery from 'vue-gallery';
 import App from './App.vue'
 Vue.use(VueFullPage);
-
+Vue.use(VueGallery);
 Vue.config.productionTip = false
 
 new Vue({
