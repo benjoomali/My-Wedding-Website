3be0b6e90e9895dc9deebecec59af60526a9c219
Ben Umali <benjoomali@gmail.com>
2019-07-17 10:07:53 -0700

    adding content, vuejs countdown, font awesome

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,89 @@
 <template>
   <div id="app">
+    <ul id="menu">
+      <li class="no-show-mobile active" data-menuanchor="home"><a href="#home"> Wedding</a></li>
+      <li data-menuanchor="ceremony"><a href="#ceremony">Ceremony</a></li>
+      <li data-menuanchor="reception"><a href="#reception">Reception</a></li>
+      <li class="no-show-mobile" data-menuanchor="story"><a href="#story">Our Story</a></li>
+      <li data-menuanchor="hotel"><a href="#hotel">Hotel</a></li>
+      <li data-menuanchor="registry"><a href="#registry">Gifts</a></li>
+  </ul>
       <full-page ref="fullpage" :options="options" id="fullpage" >
-          <div class="section">
-              <h3>The Ceremony</h3>
-              <p>St. Andrew Church in Pasadena</p>
-              <p>311 N Raymond Ave Pasadena, CA</p>
-              <p>12:30pm</p>
-          </div>
-          <div class="section">
-                <h3>The Reception</h3>
-                <p>Glen Arden Club in Glendale</p>
-                <p>357 Arden Ave Glendale, CA</p>
-                <p>4:30pm</p>
-          </div>
-          <div class="section">
-              <h3>Our Story</h3>
-              <p>Benjamin and Danielle first met in 2005 through a Catholic youth group called Couples For Christ Youth and Danielle was hosting a Halloween party at her house. Little did she know that her future husband was going to be attending this party. After many years of being friends, Danielle started her first year of college at Cal Poly Pomona while Benjamin stayed behind in Bakersfield to finish his senior year in high school. All the while, they were still one of each other's best friends throughout the year. Once Benjamin graduated high school and Danielle came back home for the summer, they began spending more time together over the summer. When Benjamin started college at University of California, Irvine in 2009, he asked Danielle's parents if he could court her. After many years of pursuing, Benjamin and Danielle continued to grow as friends, best friends and as partners. On January 27, 2018, Benjamin asked Danielle for her hand in marriage...and the rest is history!</p>
-          </div>
-          <div class="section">
-              <h3>Hotel Accomodations</h3>
-              <a href="https://www.hilton.com/en/hi/groups/personalized/B/BURHGHF-BDW-20191011/index.jhtml?WT.mc_id=POG" target="_blank">Click to Book</a>
-          </div>
-          <div class="section">
-              <h3>Our Gift Registry</h3>
-              <a href="https://www.zola.com/registry/danielleandbengetmarried" target="_blank">View our Zola Gift Registry</a>
-              <p>We would love to give all our guests the opportunity to let their hair down and have a good time without having to worry about little eyes and ears, so we politely request <strong>no children.</strong></p>
-          </div>
-          <div class="section">
-            <h3>Our Gallery</h3>
+          <div class="section" id="section0">
+            <div class="main-content">
+              <div class="overlay"></div>
+              <h1 class="main-header">Danielle & Ben</h1>
+              <h3>tie the knot</h3>
+              <span class="date">on October 12, 2019</span>
+              <hr>
+              <Countdown deadline="October 12, 2019"></Countdown>
+              <br>
+              <p class="scroll">Scroll Down for More Details</p>
+            </div>
 
+          </div>
+          <div class="section" id="section1">
+            <div class="main-content bottom-left">
+              <h1>The Ceremony</h1>
+              <h3>St. Andrew Church in Pasadena</h3>
+              <a href="https://goo.gl/maps/rJ8AXR9Cjr92" target="_blank" class="address"><font-awesome-icon icon="map-marked" /> 311 N Raymond Ave Pasadena, CA </a>
+              <br><br>
+              <a class="time" target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=M2JrcDg4MGZzYmZ1dWNpcmw2ZjQyczBoYm0gNmQwdHNiM2E4dmxnbGIwdmhwcnM5cGpzYXNAZw&tmsrc=6d0tsb3a8vlglb0vhprs9pjsas%40group.calendar.google.com">
+                <font-awesome-icon icon="clock" /> 12:30pm
+              </a>
+            </div>
+          </div>
+          <div class="section" id="section2">
+            <div class="main-content top-right">
+
+              <h1>The Reception</h1>
+              <h3>Glen Arden Club in Glendale</h3>
+              <a href="https://goo.gl/maps/KxuFDNDbDYn" class="address"><font-awesome-icon icon="map-marked" /> 357 Arden Ave Glendale, CA</a>
+              <br><br>
+              <a class="time" target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=NGt1NTNtbWRkYWNsaHYwdWVrbG5ucWc0cmUgNmQwdHNiM2E4dmxnbGIwdmhwcnM5cGpzYXNAZw&tmsrc=6d0tsb3a8vlglb0vhprs9pjsas%40group.calendar.google.com">
+                <font-awesome-icon icon="clock" /> 4:30pm
+              </a>
+            </div>
+          </div>
+          <div class="section" id="section3">
+            <div class="main-content bottom-left">
+              <h1>Our Story</h1>
+              <div class="story-text">
+                <p>Benjamin and Danielle first met in 2005 through a Catholic youth group called Couples For Christ Youth and Danielle was hosting a Halloween party at her house. Little did she know that her future husband was going to be attending this party. After many years of being friends, Danielle started her first year of college at Cal Poly Pomona while Benjamin stayed behind in Bakersfield to finish his senior year in high school. All the while, they were still one of each other's best friends throughout the year. Once Benjamin graduated high school and Danielle came back home for the summer, they began spending more time together over the summer. When Benjamin started college at University of California, Irvine in 2009, he asked Danielle's parents if he could court her. After many years of pursuing, Benjamin and Danielle continued to grow as friends, best friends and as partners. On January 27, 2018, Benjamin asked Danielle for her hand in marriage...and the rest is history!
+                </p>
+                <a class="btn" href="https://three16photography.com/disneyland-engagement-danielle-ben/?fbclid=IwAR0eH1KcJ771u0XBZitv4F-xjF8mJe7JWwzU1fmMReMvk5cymvOeqPEoHW0" target="_blank">How We Met</a>
+              </div>
+
+            </div>
+
+          </div>
+          <div class="section" id="section4">
+            <div class="main-content">
+              <h1>Hotel Accomodations</h1>
+              <p class="description">We have a hotel block at the Hilton North in Glendale, CA. This hotel is a 2 minute walk from the venue. It is in a central location near popular places in the city like the Americana, the Glendale Galleria and Max's Chicken.</p>
+              <a class="btn" href="https://www.hilton.com/en/hi/groups/personalized/B/BURHGHF-BDW-20191011/index.jhtml?WT.mc_id=POG" target="_blank" style="max-width: 300px; margin: 0 auto;"><font-awesome-icon icon="hotel" /> Click to Book</a>
+            </div>
+
+          </div>
+          <div class="section" id="section5">
+            <div class="main-content">
+              <h1>Our Gift Registry</h1>
+              <p class="description">
+                In lieu of gifts, please visit our Zola gift registry. We'll be heading to Italy for our Honeymoon, so we appreciate all the love and support!
+              </p>
+              <a class="btn" href="https://www.zola.com/registry/danielleandbengetmarried" target="_blank" style="max-width: 300px; margin: 0 auto;">View our Gift Registry</a>
+            </div>
           </div>
       </full-page>
   </div>
 </template>
 
 <script>
-import VueGallery from 'vue-gallery';
+import Countdown from 'vuejs-countdown';
 export default {
   name: 'app',
   components: {
-  'gallery': VueGallery
+    Countdown
   },
   data () {
     return {
@@ -48,39 +92,20 @@ export default {
       scrollBar: false,
       menu: '#menu',
       navigation: true,
-      anchors: ['ceremony', 'reception', 'story', 'hotel', 'registry', 'gallery'],
-      sectionsColor: ['#41b883', '#ff5f45', '#0798ec', '#fec401', '#1bcee6', '#a36b4f']
+      anchors: ['home','ceremony', 'reception', 'story', 'hotel', 'registry', 'gallery'],
+      sectionsColor: ['#89a380', '#bf7858', '#3e5835', '#b3a59c', '#466061', '#a7aea6']
     },
-    images: [
-      {
-        title: '400x400',
-        description: '400 x 400 White',
-        href: 'https://dummyimage.com/400/ffffff/000000',
-      },
-      {
-        title: '400x400',
-        description: '400 x 400 White',
-        href: 'https://dummyimage.com/400/ffffff/000000',
-      },
-      {
-        title: '400x400',
-        description: '400 x 400 Black',
-        href: 'https://dummyimage.com/400/000000/ffffff',
-      },
-      {
-        title: '400x400',
-        description: '400 x 400 Black',
-        href: 'https://dummyimage.com/400/000000/ffffff',
-      },
-    ],
-    index: null
     }
   },
+  methods: {
 
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
+    @import "./styles/app.scss";
+
     #app {
       font-family: 'Avenir', Helvetica, Arial, sans-serif;
       -webkit-font-smoothing: antialiased;
@@ -100,4 +125,8 @@ export default {
     a {
         color: #42b983;
     }
+    #fp-nav ul li a span, .fp-slidesNav ul li a span {
+        background: #fff;
+    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
 import VueFullPage from 'vue-fullpage.js'
-import VueGallery from 'vue-gallery';
+//import VueGallery from 'vue-gallery';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faClock, faMapMarked, faHotel } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
+
+library.add(faClock)
+library.add(faMapMarked)
+library.add(faHotel)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VueFullPage);
-Vue.use(VueGallery);
+//Vue.use(VueGallery);
 Vue.config.productionTip = false
 
 new Vue({
